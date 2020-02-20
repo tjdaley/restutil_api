3a62eb8d3d06693741f302054cd90077582f98be
Tom Daley <tjd@powerdaley.com>
2020-02-20 01:08:59 -0600

    add retry logic for rate limiting

--- restutil/restutil.py
+++ restutil/restutil.py
@@ -1,12 +1,14 @@
 import re
 import requests
+import time
 
 from urlmaker import UrlMaker
 
 
 class RestUtil(object):
-    def __init__(self):
-        self.urlmaker = UrlMaker(host='192.168.1.81')
+    def __init__(self, api_key: str):
+        self.urlmaker = UrlMaker(api_key, host='192.168.1.81')
+        self.auth = (api_key, None)
 
     def average_mortage_rate(self, year: int, month: int = 6, term: int = 30) -> dict:
         """
@@ -21,15 +23,37 @@ class RestUtil(object):
           (dict): Result Set the interest rate
         """
         url = self.urlmaker.url_for('fred', 'historical_rate', year=year, month=month, term=term)
-        response = requests.get(url)
-        return response.text
+        response = self.get_response(url)
+        return response.json()
+
+    def get_response(self, url: str):
+        """
+        Retrieve response from server, with retry logic.
+
+        Args:
+            url (str): URL to retrieve.
+        Returns:
+            requests.response if OK otherwise an exception.
+        """
+        rate_limit_retries_remaining = 5
+        while rate_limit_retries_remaining > 0:
+            response = requests.get(url, auth=self.auth)
+            if response.status_code == 401:
+                raise Exception(response.text)
+            response_json = response.json()
+            if response_json['success'] is True:
+                return response
+            if response_json['code'] == 'ERR_RATE_LIMIT' and rate_limit_retries_remaining > 0:
+                time.sleep(1)
+                rate_limit_retries_remaining -= 1
+        message = f"{response_json['message']} ({response_json['code']})"
+        raise Exception(message)
 
 
 def main():
-    ru = RestUtil()
-    print(ru.average_mortage_rate(2010))
-    print(ru.average_mortage_rate(2010, 10))
-    print(ru.average_mortage_rate(2010, 10, 15))
+    ru = RestUtil('tjdaley')
+    for i in range(5):
+        print(ru.average_mortage_rate(2010, i+1, 15))
 
 
 if __name__ == '__main__':
--- restutil/urlmaker.py
+++ restutil/urlmaker.py
@@ -5,12 +5,14 @@ Copyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
 """
 import re
 import requests
+import time
 
 
 class UrlMaker(object):
-    def __init__(self, host: str = 'localhost', port: int = 8081):
+    def __init__(self, api_key, host: str = 'localhost', port: int = 8081):
         self.__pattern_cache = {}
         self.__url = f'http://{host}:{port}'
+        self.__auth = (api_key, None)
 
     def url_for(self, service: str, query: str, **kwargs):
         params = kwargs
@@ -52,11 +54,30 @@ class UrlMaker(object):
     def __clear_cache(self):
         self.__pattern_cache = {}
 
-    @staticmethod
-    def __get_service_patterns(url):
-        response = requests.get(url)
-        patterns = response.json()
-        return patterns
+    def __get_service_patterns(self, url):
+        rate_limit_retries_remaining = 3
+        while rate_limit_retries_remaining > 0:
+            response = requests.get(url, auth=self.__auth)
+            if response.status_code == 401:
+                raise Exception(response.text)
+            response = response.json()
+            if response['success'] is True:
+                patterns = response['data']
+                return patterns
+            if response['code'] == 'ERR_RATE_LIMIT' and rate_limit_retries_remaining > 0:
+                time.sleep(1)
+                rate_limit_retries_remaining -= 1
+        message = f"{response['message']} ({response['code']})"
+        raise Exception(message)
+
+    def __select_service_pattern(self, patterns, params):
+        keys = params.keys()
+        for pattern in patterns:
+            if UrlMaker.__pattern_has_all_params(pattern, keys):
+                pattern_params = UrlMaker.__get_pattern_params(pattern)
+                if UrlMaker.__request_has_all_params(pattern_params, keys):
+                    return pattern, pattern_params
+        return None, None
 
     @staticmethod
     def __get_pattern_params(pattern):
@@ -88,16 +109,6 @@ class UrlMaker(object):
             if p not in request_params:
                 return False
         return True
-
-    @staticmethod
-    def __select_service_pattern(patterns, params):
-        keys = params.keys()
-        for pattern in patterns:
-            if UrlMaker.__pattern_has_all_params(pattern, keys):
-                pattern_params = UrlMaker.__get_pattern_params(pattern)
-                if UrlMaker.__request_has_all_params(pattern_params, keys):
-                    return pattern, pattern_params
-        return None, None
 
     @staticmethod
     def __make_url(url, pattern_params, query_params):
